40342bc0159485684e49264498541f4a9f779a28
Patrick Gagnon-Renaud <pgrenaud@among-us.app>
2020-09-26 21:51:47 -0400

    Frontend: Add vue-socket.io scaffolding

--- src/frontend/main.js
+++ src/frontend/main.js
@@ -3,8 +3,23 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import VueSocketIO from "vue-socket.io";
 
 Vue.config.productionTip = false;
+
+let hostname = window.location.hostname;
+
+Vue.use(
+  new VueSocketIO({
+    debug: true,
+    connection: "http://" + hostname + ":3000",
+    vuex: {
+      store,
+      actionPrefix: "SOCKET_",
+      mutationPrefix: "SOCKET_"
+    }
+  })
+);
 
 new Vue({
   router,
--- src/frontend/store/index.js
+++ src/frontend/store/index.js
@@ -4,8 +4,51 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    isConnected: false
+  },
+  mutations: {
+    SOCKET_connect(state) {
+      state.isConnected = true;
+    },
+    SOCKET_disconnect(state) {
+      state.isConnected = false;
+    }
+  },
+  actions: {
+    SOCKET_connect() {
+      console.log("action SOCKET_connect");
+    },
+    SOCKET_error() {
+      console.log("action SOCKET_error");
+    },
+    SOCKET_disconnect() {
+      console.log("action SOCKET_disconnect");
+    },
+    SOCKET_reconnect() {
+      console.log("action SOCKET_reconnect");
+    },
+    SOCKET_reconnect_attempt() {
+      console.log("action SOCKET_reconnect_attempt");
+    },
+    SOCKET_reconnecting() {
+      console.log("action SOCKET_reconnecting");
+    },
+    SOCKET_reconnect_error() {
+      console.log("action SOCKET_reconnect_error");
+    },
+    SOCKET_reconnect_failed() {
+      console.log("action SOCKET_reconnect_failed");
+    },
+    SOCKET_connect_error() {
+      console.log("action SOCKET_connect_error");
+    },
+    SOCKET_connect_timeout() {
+      console.log("action SOCKET_connect_timeout");
+    },
+    SOCKET_connecting() {
+      console.log("action SOCKET_connecting");
+    }
+  },
   modules: {}
 });
