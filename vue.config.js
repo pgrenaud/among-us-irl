752c81446f2011478498d7b3b4cb1e96ed0e0370
Patrick Gagnon-Renaud <pgrenaud@among-us.app>
2020-09-26 20:25:10 -0400

    Frontend: Move all frontend sources to src/frontend

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,16 @@
+const path = require("path");
+
 module.exports = {
+  chainWebpack: config => {
+    config
+      .entry("app")
+      .clear()
+      .add("./src/frontend/main.js")
+      .end();
+    config
+      .resolve
+      .alias
+      .set("@", path.resolve(__dirname, "src/frontend"));
+  },
   transpileDependencies: ["vuetify"]
 };
