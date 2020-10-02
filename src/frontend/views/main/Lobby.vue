1e37961327572f718b023708b69e9a9231589d4d
Patrick Gagnon-Renaud <pgrenaud@among-us.app>
2020-10-01 21:52:37 -0400

    Frontend: Add user list in lobby

--- src/frontend/views/main/Lobby.vue
+++ src/frontend/views/main/Lobby.vue
@@ -1,11 +1,55 @@
 <template>
   <v-container fluid>
+    <v-list>
+      <v-list-item v-for="user in users" :key="user.id">
+        <v-list-item-avatar>
+          <v-avatar color="grey" size="36">
+            <span class="white--text headline">{{ user.name.charAt(0) }}</span>
+          </v-avatar>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title v-text="user.name"></v-list-item-title>
+        </v-list-item-content>
+
+        <v-list-item-action>
+          <v-btn icon color="green">
+            <v-icon>mdi-check</v-icon>
+          </v-btn>
+        </v-list-item-action>
+        <v-list-item-action>
+          <v-btn icon color="red">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-list-item-action>
+      </v-list-item>
+    </v-list>
   </v-container>
 </template>
 
 <script>
 export default {
   name: "Lobby",
+  data: () => ({
+    users: [
+      {
+        id: 1,
+        name: "Bob Graton"
+      },
+      {
+        id: 2,
+        name: "Alice Graton"
+      },
+      {
+        id: 3,
+        name: "Joe Graton"
+      },
+      {
+        id: 4,
+        name: "Sylvie Graton"
+      }
+    ]
+  }),
   mounted() {
     this.$navigation.setTitle("Lobby");
     this.$loading.hideSpinner();
